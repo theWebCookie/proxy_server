c1ff6396d9311f764e9f15e04d68d6af11344f4f
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-10-07 19:04:14 +0200

    refactor url to use env variables

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const nasaUrl = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2024-06-30&end_date=2024-07-04&api_key=${process.env.NASA_API_KEY}`;
+const nasaUrl = `${process.env.NASA_API_URL}?start_date=${process.env.START_DATE}&end_date=${process.env.END_DATE}&api_key=${process.env.NASA_API_KEY}`;
 
 axios
   .get(nasaUrl)
   .then((response) => {
     const data = response.data;
-    console.log(JSON.stringify(data, 0, 1));
+    console.log(JSON.stringify(data, null, 2));
     console.log('Number of asteroids seen:', data.element_count);
   })
   .catch((error) => {
