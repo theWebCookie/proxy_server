69a4741da73a3bcd4116d71443eb5d37649f54f9
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-10-07 19:50:00 +0200

    add get logic for meteors

--- index.js
+++ index.js
@@ -16,8 +16,23 @@ const nasaUrl = `${process.env.NASA_API_URL}?start_date=${process.env.START_DATE
 app.get('/meteors', async (req, res) => {
   try {
     const response = await axios.get(nasaUrl);
-    const meteorData = response.data;
-    res.json(meteorData);
+    const meteorData = response.data.near_earth_objects;
+
+    const meteorList = [];
+    for (const date in meteorData) {
+      meteorData[date].forEach((meteor) => {
+        meteorList.push({
+          id: meteor.id,
+          name: meteor.name,
+          diameter_meters: meteor.estimated_diameter.meters.estimated_diameter_max,
+          is_potentially_hazardous_asteroid: meteor.is_potentially_hazardous_asteroid,
+          close_approach_date_full: meteor.close_approach_data[0].close_approach_date_full,
+          relative_velocity_km_per_sec: meteor.close_approach_data[0].relative_velocity.kilometers_per_second,
+        });
+      });
+    }
+
+    res.json(meteorList);
   } catch (error) {
     console.error('Error fetching meteor data:', error);
     res.status(500).json({ error: 'Error fetching meteor data' });
