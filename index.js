e183b50cb4eab0bd5af70cd559605c5aa67da208
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-10-07 19:26:59 +0200

    first endpoint

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
+const nasaUrl = `${process.env.NASA_API_URL}?start_date=${process.env.START_DATE}&end_date=${process.env.END_DATE}&api_key=${process.env.NASA_API_KEY}`;
+
+app.get('/meteors', async (req, res) => {
+  try {
+    const response = await axios.get(nasaUrl);
+    const meteorData = response.data;
+    res.json(meteorData);
+  } catch (error) {
+    console.error('Error fetching meteor data:', error);
+    res.status(500).json({ error: 'Error fetching meteor data' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-const nasaUrl = `${process.env.NASA_API_URL}?start_date=${process.env.START_DATE}&end_date=${process.env.END_DATE}&api_key=${process.env.NASA_API_KEY}`;
-
-axios
-  .get(nasaUrl)
-  .then((response) => {
-    const data = response.data;
-    console.log(JSON.stringify(data, null, 2));
-    console.log('Number of asteroids seen:', data.element_count);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
