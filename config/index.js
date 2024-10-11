24af5d1eed27d699a326c29fddcfbf0afe194d4d
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-10-11 15:10:36 +0200

    fix review issues

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  nasaUrl: `${process.env.NASA_API_URL}?api_key=${process.env.NASA_API_KEY}`,
-  roverUrl: `${process.env.ROVER_API_URL}`,
+  nasaUrl: `${process.env.NASA_API_BASE_URL}/neo/rest/v1/feed?api_key=${process.env.NASA_API_KEY}`,
+  roverUrl: `${process.env.NASA_API_BASE_URL}/mars-photos/api/v1/rovers/curiosity/photos`,
   port: process.env.PORT || 4000,
 };
