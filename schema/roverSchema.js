b6bb31b9e98d4ba3c3bcbffd8a82a58e7442fe2d
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-10-14 13:16:10 +0200

    fix rover schema

--- schema/roverSchema.js
+++ schema/roverSchema.js
@@ -1,11 +1,8 @@
 import Joi from 'joi';
 
 export const roverSchema = Joi.object({
-  sol: Joi.number().integer().positive().required().messages({
-    'any.required': 'sol is required',
-    'number.base': 'sol must be a valid number',
-    'number.positive': 'sol must be a positive number',
-  }),
+  userId: Joi.number().required(),
+  userName: Joi.string().required(),
   apiKey: Joi.string().required().messages({
     'any.required': 'API key is required',
     'string.base': 'API key must be a valid string',
