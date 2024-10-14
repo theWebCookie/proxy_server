da999f89b4f7521315d53caa353a1f6dde9e1421
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-10-14 12:43:57 +0200

    use exception

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,14 +1,12 @@
+import Exception from '../utils/exception.js';
+
 export const validate = (schema) => {
   return (req, res, next) => {
     const data = req.method === 'GET' ? req.query : req.body;
     const { error } = schema.validate(data, { abortEarly: true });
 
     if (error) {
-      return res.status(400).json({
-        status: 'Fail',
-        message: 'Validation error',
-        details: error.details.map((detial) => detial.message),
-      });
+      throw new Exception(400, `Validation error: ${error.details.map((detial) => detial.message)}`);
     }
 
     next();
